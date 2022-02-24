6e90028b5827e65f27ee87dfa49c19ff09cfa4ec
Michael Clermont <michael.clermont@mths.ca>
2022-02-24 19:23:33 +0000

    added math.py

---
+++ math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created by: Michael Clermont
+# Created on: Feb 2022
+# This program calculates the area and perimeter of a rectangle
+#    with dimensions 5cm x 3cm
+
+
+def main():
+    # this function calculates the area and perimeter
+
+    print("If a rectangle has the dimensions: ")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm².".format(3 * 5))
+    print("Perimeter is {}cm.".format(2 * (3 + 5)))
+
+
+if __name__ == "__main__":
+    main()
